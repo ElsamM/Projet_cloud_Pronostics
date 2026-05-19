8c099c59c3ad419ce9d7800dbda28eac63e78048
Elsa <elsamariemoh@gmail.com>
2026-05-19 18:23:24 +0200

    Nettoyage du fichier reset_db

--- reset_db.py
+++ reset_db.py
@@ -14,22 +14,7 @@ conn = sqlite3.connect('database.db')
 with open('schema.sql', 'r', encoding='utf-8') as f:
     conn.executescript(f.read())
 
-# 3. Ajouter des joueurs avec Vrais Noms / Prénoms
-print("👤 Création des joueurs avec Noms et Prénoms...")
-joueurs = [
-    ("Mbappé", "Kylian", "Kiki", "kylian@esme.fr", "mdp123", 36),
-    ("Renard", "Wendie", "CaptainW", "wendie@esme.fr", "mdp123", 31),
-    ("Griezmann", "Antoine", "Grizou", "antoine@esme.fr", "mdp123", 30),
-    ("Zidane", "Zinédine", "Zizou", "zinedine@esme.fr", "mdp123", 26)
-]
-
-for j in joueurs:
-    conn.execute('''
-        INSERT INTO Utilisateurs (nom, prenom, pseudo, email, mot_de_passe_hash, points_totaux)
-        VALUES (?, ?, ?, ?, ?, ?)
-    ''', (j[0], j[1], j[2], j[3], generate_password_hash(j[4]), j[5]))
-
-# 4. Recréer le compte Administrateur
+# 3. Recréer le compte Administrateur (Obligatoire pour gérer le site)
 conn.execute('''
     INSERT INTO Utilisateurs (nom, prenom, pseudo, email, mot_de_passe_hash) 
     VALUES (?, ?, ?, ?, ?)
@@ -38,4 +23,4 @@ conn.execute('''
 conn.commit()
 conn.close()
 
-print("✅ Base de données recréée à NEUF avec les nouveaux joueurs !")
+print("✅ Base de données recréée à NEUF, prête pour les vrais joueurs !")
